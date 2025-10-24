156d305447bbc5355363e8a79f4c5533bee4031b
harshitclub <harshitclub@gmail.com>
2025-10-24 12:05:01 +0530

    feat: added lazy loading for routes

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,17 +1,18 @@
 import Navbar from "../components/global/navbar/Navbar";
 import Footer from "../components/global/footer/Footer";
-import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 
-export default function MainLayout() {
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <div className="min-h-screen flex flex-col">
           <Navbar />
-          <main className="grow">
-            <Outlet />
-          </main>
+          <main className="grow">{children}</main>
           <Footer />
         </div>
       </ThemeProvider>
